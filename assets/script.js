da4c3c969761dbf062c513d727dc396ee15b55f5
Kristin Gumbert <kristingumbert@Kristins-MacBook-Pro.local>
2016-06-11 01:01:23 -0400

    minor

--- assets/script.js
+++ assets/script.js
@@ -7,22 +7,22 @@
 			var date = $('#eventDate').val().trim();
 			var time = $('#eventTime').val().trim();
 			var location = $('#eventLocation').val().trim();
+			var countdown = 0;
 
 			console.log(name);
 
 
+			$('#eventData').prepend("<tr>"+
+						"<td>"+name+"</td>"+
+						"<td>"+date+"</td>"+
+						"<td>"+time+"</td>"+
+						"<td>"+location+"</td>"+
+						"<td>"+countdown+"</td>"+
+					"</tr>");
+
+
+
 		});
-
-
-
-
-
-
-
-
-
-
-
 
 
 
